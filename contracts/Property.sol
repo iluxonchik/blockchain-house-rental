c4b4a84a7a965aeb1cf27cd9c5d90d1a99dbb373
Illya Gerasymchuk <illya@iluxonchik.me>
2022-08-12 02:43:36 +0100

    feat: implement real estate property smart contract
    Test ERC-721 is put on the blockchain and tested on OpenSea: https://testnets.opensea.io/assets/rinkeby/0x8825d3a32115878ce4f963fb9add452d40d2d5bf/0

--- contracts/Property.sol
+++ contracts/Property.sol
@@ -1,10 +1,18 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
+
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract Property is ERC721URIStorage{
-    constructor() ERC721("HITMOTS", "House In The Middle Of The Street") {
-       // TODO: set URI here and mint
+//metadataUri=ipfs://QmNniGfFDw43dRsX9JxYTafyLx9gybkupvn52KpXsQvYW5
+
+/// @notice Represents a real estate property in the system.
+contract Property is ERC721URIStorage {
+    uint256 tokenId;
+    constructor(string memory name_, string memory symbol_, string memory metadataUri_) ERC721(name_, symbol_) {
+        // only one token will be created in this ERC721, which is the property itself
+        tokenId = 0;
+        _mint(msg.sender, tokenId);
+        _setTokenURI(tokenId, metadataUri_);
     }
 }
